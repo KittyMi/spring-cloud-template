c58e80f81e5e1a45f6b67aa6cc2ad05703e94df1
andy_lai <641230056@qq.com>
2021-03-26 17:05:31 +0800

    --新增 seata 模块 TODO 暂时还有问题要完善  等忙完这个项目在完善

--- biz-mybatis/src/main/java/com/andy/mybatis/config/DefaultMybatisPlusConfig.java
+++ biz-mybatis/src/main/java/com/andy/mybatis/config/DefaultMybatisPlusConfig.java
@@ -1,13 +1,11 @@
 package com.andy.mybatis.config;
 
 import com.baomidou.mybatisplus.extension.plugins.PaginationInterceptor;
-import com.baomidou.mybatisplus.extension.plugins.PerformanceInterceptor;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.DependsOn;
-import org.springframework.context.annotation.Profile;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 /**
@@ -31,13 +29,4 @@ public class DefaultMybatisPlusConfig {
         return new PaginationInterceptor();
     }
 
-    /**
-     * 性能分析插件仅在开发环境中使用
-     * @return 性能分析插件
-     */
-    @Bean
-    @Profile({"dev", "default"})
-    public PerformanceInterceptor performanceInterceptor() {
-        return new PerformanceInterceptor();
-    }
 }
